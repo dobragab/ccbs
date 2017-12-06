06bbab386c3891f0fe8a3f28ab52e421a3e35f7a
Gábor Dobra <dobragab@gmail.com>
2017-12-06 06:04:07 +0100

    BSmart

--- program/main.cpp
+++ program/main.cpp
@@ -13,9 +13,10 @@ int main(int argc, const char ** argv)
         return 1;
     }
 
-    auto self_include_dir = "-I" + ccsh::fs::weakly_canonical(CCBS_DOT / "../include"_p).string();
+    ccsh::fs::path self_include_dir = ccsh::fs::weakly_canonical(CCBS_DOT / "../include"_p);
 
     ccsh::command_builder<ccsh::gcc> pragma_command = ccsh::gcc();
+    pragma_command.I(self_include_dir);
     pragma_command.args().push_back(self_include_dir);
     pragma_command.args().emplace_back(argv[1]);
     pragma_command.args().emplace_back("-E");
@@ -36,26 +37,31 @@ int main(int argc, const char ** argv)
     if (dot != std::string::npos)
         classname = classname.substr(0, dot);
 
+
     ccsh::fs::path output = "/tmp/"_p / ccsh::generate_filename();
 
-    std::vector<std::string> args = {
-        "-D_CCBS_CLASSNAME_AUTOGEN=" + classname,
-        self_include_dir,
-        "-L" + (CCBS_DOT / "../lib"_p).string(),
-        "-Wl,-rpath," + (CCBS_DOT / "../lib"_p).string(),
-        "-lccbs_lib",
-        "-L" + (CCBS_DOT / "../../ccsh/lib"_p).string(),
-        "-Wl,-rpath," + (CCBS_DOT / "../../ccsh/lib"_p).string(),
-        "-lccsh_lib",
-        "-I" + ccsh::fs::absolute(header).parent_path().string(),
-        "-o",
-        output.string(),
-        "-x", "c++",
-        "-" // read from stdin
-    };
+    ccsh::command_builder<ccsh::gcc> gcc = ccsh::gcc("-");
+
+    gcc.D("_CCBS_CLASSNAME_AUTOGEN", classname);
+    gcc.I(self_include_dir);
+    gcc.I(ccsh::fs::absolute(header).parent_path());
+
+    // TODO: make these configurable
+    gcc.L(CCBS_DOT / "../lib"_p);
+    gcc.rpath(CCBS_DOT / "../lib"_p);
+    gcc.l("ccbs_lib");
+
+    gcc.L(CCBS_DOT / "../../ccsh/lib"_p);
+    gcc.rpath(CCBS_DOT / "../../ccsh/lib"_p);
+    gcc.l("ccsh_lib");
+
+    gcc.o(output);
+
+    gcc.args().emplace_back("-x");
+    gcc.args().emplace_back("c++");
 
     for (const auto& dir : includes)
-        args.push_back("-I" + dir.string());
+        gcc.I(dir);
 
     std::string program =
     "#include <" + header.string() + ">\n" +
@@ -66,9 +72,7 @@ int main()
 }
     )PROGRAM";
 
-    ccsh::command gcc_command = ccsh::shell(ccbs::get_compiler_path(), args);
-
-    result = (gcc_command < program).run();
+    result = (gcc < program).run();
     if (result != 0)
         return result;
 
